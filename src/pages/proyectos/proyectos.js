21ef44ac034c79f0afd46920e6a645884aefb895
dani <>
2021-05-08 19:04:59 +0200

    SR

--- src/pages/proyectos/proyectos.js
+++ src/pages/proyectos/proyectos.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import styles from './proyectos.module.css';
+import React from "react";
+import styles from "./proyectos.module.css";
+import proweb21 from "../../media/logolargow.png";
+import repro from "../../media/logonew2.png";
+import cifras from "../../media/logo.png";
 
 const Proyectos = () => {
-
-
-    return ( 
-            <div>
-                <p>Proyectos  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam consequuntur maiores aspernatur eligendi, ipsa itaque accusantium corrupti ducimus rerum perferendis laudantium qui quasi! Voluptatibus explicabo facilis sed repudiandae! Quam voluptates similique tempore quis omnis soluta voluptas, facilis quisquam non nesciunt voluptatibus, quidem consequuntur dignissimos quasi nisi. Perspiciatis necessitatibus animi quis voluptate facere, maiores laboriosam vero suscipit magni repudiandae, quibusdam amet mollitia reprehenderit adipisci aspernatur deleniti possimus, totam quaerat ullam inventore neque ad numquam accusamus. Quas modi officiis repudiandae vel ipsa beatae ipsam voluptatum optio dicta, sit provident aperiam? Beatae dolorum illo obcaecati sequi suscipit maxime non eveniet mollitia, placeat cum expedita praesentium commodi asperiores unde dicta ab aliquam saepe veritatis omnis similique. Harum, accusamus dolore sequi eveniet consequuntur est et nam, illo quo tenetur quasi eaque dicta earum sint esse! Pariatur, eius? Dicta harum earum deserunt doloribus itaque esse, non rem quae nihil exercitationem adipisci, suscipit accusantium maxime assumenda saepe.</p>
-            </div>
-    )
-}
+  return (
+    <div className={styles.__proyectos}>
+      <h2 className={styles.__proyectos_title}>
+        Algunos proyectos en los que estoy inmerso
+      </h2>
+      <div className={styles.__proyectos_wrapper}>
+        <div className={styles.__proyecto}>
+          <img className={styles.__proyectos_img} src={proweb21} alt="" />
+        </div>
+        <div className={styles.__proyecto}>
+          <img className={styles.__proyectos_img} src={repro} alt="" />
+        </div>
+        <div className={styles.__proyecto}>
+          <img className={styles.__proyectos_img} src={cifras} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Proyectos;

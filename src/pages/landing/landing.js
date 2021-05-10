fec829c04c4a0ae48a848a87b8db29c6e9a4b8cd
dani <>
2021-05-10 17:09:29 +0200

    laptop

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./landing.module.css";
-import daniel from "../../media/daniel.jpeg";
+import daniel from "../../media/daniperfil.png";
 
 const Landing = () => {
+  
   return (
-    <div className={styles.__sobremi_foto_div}>
+    <div  className={styles.__sobremi_foto_div}>
+    
       <img
         className={styles.__sobremi_foto}
         src={daniel}
         alt="Foto Daniel Andrés"
       />
+      <h1 className={styles.__sobremi_saludo}>HOLA,<br/>SOY DANI</h1>
     </div>
   );
 };
